6ac5e7093fcfcdc9809361af5777cd65cabc05c2
Roggers Anguzu <141458053+RoggersAnguzu@users.noreply.github.com>
2023-08-05 02:06:58 +0300

    Add files via upload
    This is a Simple Matrix code that gets the sum of Two matrices

---
+++ SUM OF MATRIX/CREATING A MATRIX USING A 2D Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Matrix
+{
+    public class Mats
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the rows and Columns of Matrices:");
+            int Rows = Convert.ToInt32(Console.ReadLine());
+            int Columns = Convert.ToInt32(Console.ReadLine());
+            int[,] Matrix1 = new int[Rows, Columns];
+            int[,] Matrix2 = new int[Rows, Columns];
+            int[,] ResultMatrix = new int[Rows, Columns];
+            //Enter Matrix 1 Elements
+            Console.WriteLine("\nEnter the elements of the first Matrix: ");
+            for(int i=0; i<Rows;i++)
+            {
+                for(int j = 0; j<Columns; j++)
+                {
+                    Matrix1[i, j] = int.Parse(Console.ReadLine());
+                }
+                
+            }
+            //Enter the Matrix 2 Elements
+            Console.WriteLine("\nEnter the elements of the Second Matrix: ");
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Matrix2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nSum of both the Matrices:");
+            for(int i =0; i<Rows; i++)
+            {
+                for(int j = 0; j<Columns; j++)
+                {
+                    ResultMatrix[i, j] = Matrix1[i, j] + Matrix2[i, j];
+                    Console.Write($"{ResultMatrix[i, j]}");
+                }
+                Console.WriteLine("Please press the Enter button to Exit!");
+            }
+        }
+    }
+}
